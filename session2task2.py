094b3d75182735e20a9df99acec4361caf1c95ca
YeganehValizade <152441242+YeganehValizade@users.noreply.github.com>
2023-12-01 00:34:42 +0330

    Create session2task2.py

---
+++ session2task2.py
@@ -0,0 +1,11 @@
+symbol = input("choose a symbol: (+,-,*,/):")
+a =int(input("enter the first number: "))
+b =int (input("enter the second number: "))
+if symbol == ("+"):
+    print(a+b)
+if symbol ==("-"):
+    print(a-b)
+if symbol==("*"):
+    print(a*b)
+if symbol==("/"):
+    print(a/b)
