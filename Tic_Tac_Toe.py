0a4fc8c73a5a6ee5b40e3101ca34a224145a6e06
Yeganeh <152441242+YeganehValizade@users.noreply.github.com>
2023-12-24 17:30:10 +0330

    Update Tic_Tac_Toe.py

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,27 +1,51 @@
-game_board = [["-", "-", "-"],
-              ["-", "-", "-"],
-              ["-", "-", "-"]]
-for row in game_board:
-    for cel in row:
-        print(cel, end=" ")
-    print()
+def show():
+    for row in game:
+        for cel in row:
+            print(cel, end = " ")
+        print()
 
-#player1 => X
-print("player1")
-row = int(input("enter row: "))
-cel = int(input("enter cel:"))
-game_board[row][cel] = "X"
-for row in game_board:
-    for cel in row:
-        print(cel, end = " ")
-    print()
+def check_game():
+    flag = 0
+    if game[0][0] == "X" and game[0][1] == "X" and game[0][2] == "X":
+        print("player2 win!")
+        flag = 1
+    if game[0][0] == "O" and game[0][1] == "O" and game[0][2] == "O":
+        print("player1 win!")
+        flag = 1
 
-#player2 => O
-print("player2")
-row = int(input("enter row: "))
-cel = int(input("enter cel:"))
-game_board[row][cel] = "O"
-for row in game_board:
-    for cel in row:
-        print(cel, end = " ")
-    print()
+game = [["-", "-", "-"],
+        ["-", "-", "-"],
+        ["-", "-", "-"]]
+show()
+while True:
+    print("player1")
+    while True:
+        row = int(input("enter row:"))
+        cel = int(input("enter cel:"))
+        if 0 <= row <= 2 and 0 <= cel <= 2:
+            if game[row][cel] == "-":
+                game[row][cel] = "O"
+                show()
+                check = check_game()
+                break
+            else: print("choose another cel")
+        else:
+            print("choose between 0 and 2")
+    if check == 1:
+     break
+
+    print("player2")
+    while True:
+            row = int(input("enter row: "))
+            cel = int(input("enter cel: "))
+            if 0 <= row <= 2 and 0 <= cel <= 2:
+                if game[row][cel] == "-":
+                    game[row][cel] = "X"
+                    show()
+                    check_game()
+                    break
+                else: print("choose another cel")
+            else:
+                print("choose between 0 and 2")            
+    if check == 1:
+     break
